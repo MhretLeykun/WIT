33143f7285090608e23e84101f5a0f2f6b6c3d2a
Alem <myoni7634@gmail.com>
2020-09-15 21:47:02 +0700

    installed packges

--- public/js/app.js
+++ public/js/app.js
@@ -55563,8 +55563,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Laravel\wit7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Laravel\wit7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\code\social\WIT\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\code\social\WIT\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
